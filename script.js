cff1cf9eac5310e243bd785e9885bbb13edc3982
Prefeito-iele <raphaelgcosta2010@gmail.com>
2025-04-23 16:45:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,76 @@
 const cartas = [
-  '4♦️', '5♣️', '6♠️', '7♥️', 'Q♣️',
-  'J♦️', 'K♠️', 'A♥️', '2♠️', '3♣️'
+  '4D', '5C', '6S', '7H', 'QC', 'JD', 'KS', 'AH', '2S', '3C'
 ];
 
-const imagens = {
-  '4♦️': 'https://deckofcardsapi.com/static/img/4D.png',
-  '5♣️': 'https://deckofcardsapi.com/static/img/5C.png',
-  '6♠️': 'https://deckofcardsapi.com/static/img/6S.png',
-  '7♥️': 'https://deckofcardsapi.com/static/img/7H.png',
-  'Q♣️': 'https://deckofcardsapi.com/static/img/QC.png',
-  'J♦️': 'https://deckofcardsapi.com/static/img/JD.png',
-  'K♠️': 'https://deckofcardsapi.com/static/img/KS.png',
-  'A♥️': 'https://deckofcardsapi.com/static/img/AH.png',
-  '2♠️': 'https://deckofcardsapi.com/static/img/2S.png',
-  '3♣️': 'https://deckofcardsapi.com/static/img/3C.png'
-};
+const imagens = carta => `https://deckofcardsapi.com/static/img/${carta}.png`;
+
+let pontosJogador = 0;
+let pontosCPU = 0;
 
 const mesa = document.getElementById('mesa');
-const botao = document.getElementById('sortear');
+const maoJogador = document.getElementById('maoJogador');
+const btnNovaRodada = document.getElementById('novaRodada');
+const spanJogador = document.getElementById('pontosJogador');
+const spanCPU = document.getElementById('pontosCPU');
 
-function sortearCartas() {
-  mesa.innerHTML = ''; // limpa as cartas antigas
+btnNovaRodada.addEventListener('click', iniciarRodada);
 
-  const embaralhadas = cartas.sort(() => 0.5 - Math.random());
-  const selecionadas = embaralhadas.slice(0, 3);
+function iniciarRodada() {
+  mesa.innerHTML = '';
+  maoJogador.innerHTML = '';
 
-  selecionadas.forEach(carta => {
-    const divCarta = document.createElement('div');
-    divCarta.className = 'carta';
-    divCarta.style.backgroundImage = `url(${imagens[carta]})`;
-    mesa.appendChild(divCarta);
+  const baralho = [...cartas].sort(() => 0.5 - Math.random());
+  const mao = baralho.slice(0, 6);
+  const maoJog = mao.slice(0, 3);
+  const maoComp = mao.slice(3, 6);
+
+  maoJog.forEach((carta, index) => {
+    const div = document.createElement('div');
+    div.className = 'carta';
+    div.style.backgroundImage = `url(${imagens(carta)})`;
+    div.onclick = () => jogarCarta(carta, maoComp[index]);
+    maoJogador.appendChild(div);
   });
 }
 
-botao.addEventListener('click', sortearCartas);
+function jogarCarta(cartaJog, cartaComp) {
+  maoJogador.innerHTML = '';
+  mesa.innerHTML = '';
 
+  const cartaJogEl = document.createElement('div');
+  cartaJogEl.className = 'carta';
+  cartaJogEl.style.backgroundImage = `url(${imagens(cartaJog)})`;
+
+  const cartaCompEl = document.createElement('div');
+  cartaCompEl.className = 'carta';
+  cartaCompEl.style.backgroundImage = `url(${imagens(cartaComp)})`;
+
+  mesa.appendChild(cartaJogEl);
+  mesa.appendChild(cartaCompEl);
+
+  if (cartas.indexOf(cartaJog) > cartas.indexOf(cartaComp)) {
+    pontosJogador++;
+  } else {
+    pontosCPU++;
+  }
+
+  spanJogador.textContent = pontosJogador;
+  spanCPU.textContent = pontosCPU;
+
+  if (pontosJogador >= 12) {
+    alert("Você venceu o jogo!");
+    resetarJogo();
+  } else if (pontosCPU >= 12) {
+    alert("O computador venceu o jogo!");
+    resetarJogo();
+  }
+}
+
+function resetarJogo() {
+  pontosJogador = 0;
+  pontosCPU = 0;
+  spanJogador.textContent = '0';
+  spanCPU.textContent = '0';
+  mesa.innerHTML = '';
+  maoJogador.innerHTML = '';
+}
